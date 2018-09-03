583455732c1fae14f13f05e2ce4f5f8b4a68adfa
Adam <adouglas@protonmail.com>
2018-09-03 23:14:19 +1000

    Player end point now extracts required data from quake API response.

--- tools/quakestats/index.js
+++ tools/quakestats/index.js
@@ -1,15 +1,4 @@
-const axios = require('axios');
-const apiEndpoint = 'https://stats.quake.com/api/v2';
-
-const player = alias => {
-  const endpoint = `${apiEndpoint}/Player/Stats`;
-  const query = `?name=${alias}`;
-  const url = `${endpoint}${query}`;
-  return axios
-    .get(url)
-    .then(res => res.data)
-    .catch(err => err);
-};
+const player = require('./player');
 
 module.exports = {
   player
---
+++ tools/quakestats/player.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const apiEndpoint = require('./common').apiEndpoint;
+
+const player = alias => {
+  const endpoint = `${apiEndpoint}/Player/Stats`;
+  const query = `?name=${alias}`;
+  const url = `${endpoint}${query}`;
+  return axios
+    .get(url)
+    .then(res => ({
+      alias: res.data.name,
+      duelRating: res.data.playerRatings.duel.rating,
+      level: res.data.playerLevelState.level,
+      gamesCount: res.data.playerRatings.duel.gamesCount,
+      wins: getWins(res.data.playerProfileStats.champions),
+      favoriteChampions: getFavoriteChampions(
+        res.data.playerProfileStats.champions
+      ),
+      weaponStats: getWeaponStats(res.data.playerProfileStats.champions)
+    }))
+    .catch(err => err);
+};
+
+const getWins = champStats => {
+  let total = 0;
+  for (champion in champStats) {
+    total += champStats[champion].gameModes.DUEL.won;
+  }
+  return total;
+};
+
+const getFavoriteChampions = champStats => {
+  let favorites = [];
+  for (champion in champStats) {
+    favorites.push({
+      name: champion,
+      timePlayed: champStats[champion].gameModes.DUEL.timePlayed
+    });
+  }
+  return favorites.sort((a, b) => b.timePlayed - a.timePlayed).splice(0, 3);
+};
+
+const getWeaponStats = champStats => {
+  let weaponStats = {
+    GAUNTLET: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    MACHINEGUN: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    MACHINEGUN_GRADE1: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    SHOTGUN: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    SHOTGUN_GRADE1: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    NAILGUN: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    NAILGUN_GRADE1: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    ROCKET_LAUNCHER: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    LIGHTNING_GUN: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    RAILGUN: { hits: 0, shots: 0, kills: 0, damage: 0 },
+    LAGBOLT: { hits: 0, shots: 0, kills: 0, damage: 0 }
+  };
+
+  for (champion in champStats) {
+    const weapons = champStats[champion].damageStatusList;
+    for (weapon in weapons) {
+      if (weaponStats.hasOwnProperty(weapon)) {
+        weaponStats[weapon].hits += weapons[weapon].hits;
+        weaponStats[weapon].shots += weapons[weapon].shots;
+        weaponStats[weapon].kills += weapons[weapon].kills;
+        weaponStats[weapon].damage += weapons[weapon].damage;
+      }
+    }
+  }
+  return weaponStats;
+};
+
+module.exports = player;
