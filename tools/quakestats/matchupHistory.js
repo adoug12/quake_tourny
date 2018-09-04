e14b2fb110f0c29fbe46d1e3959b3b905a4dc89d
Adam <adam.douglas@connect.qut.edu.au>
2018-09-04 16:31:56 +1000

    Added some comments for some less than obvious code

--- tools/quakestats/matchupHistory.js
+++ tools/quakestats/matchupHistory.js
@@ -2,14 +2,18 @@ const axios = require('axios');
 const apiEndpoint = require('./common').apiEndpoint;
 
 const matchupHistory = (alias1, alias2) => {
+  // Get game history of first player as array
   return games(alias1)
     .then(data =>
+      // Request full data on all games, replacing the summary data in the array
       Promise.all(
         data.map(match =>
           game(match.id)
             .then(data => data)
             .catch(err => err)
         )
+        // results is the array of matches. Each match has a battleReportPersonalStatistics array containing player objects
+        // The results array is filtered based on the presence of the second player in the battleReportPersonalStatistics array
       ).then(results =>
         results.filter(
           result =>
